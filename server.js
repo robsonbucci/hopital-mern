d82e7ec5e17356fe8e237d17119b72f50829a2a5
Robson Messias <robsonbucci@hotmail.com>
2024-07-17 22:44:02 -0300

    setup cloudinary

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 import app from './app.js';
+import cloudnary from 'cloudinary';
+
 const port = process.env.PORT;
+
+cloudnary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_CLOUD_KEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_SECRET,
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
