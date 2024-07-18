065d2026e297e0ebb966c0cf40922224d28c79fe
Robson Messias <robsonbucci@hotmail.com>
2024-07-17 23:03:16 -0300

    setup controller

---
+++ controller/messageController.js
@@ -0,0 +1,22 @@
+import { Message } from '../models/messageSchema';
+
+export const sendMessage = async (req, res, next) => {
+  const { firstName, lastName, email, phone, message } = req.body;
+  if (!firstName || !lastName || !email || !phone || !message) {
+    return res.status(400).json({
+      success: false,
+      message: 'Please Fill Full Form',
+    });
+  }
+  await Message.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    message,
+  });
+  res.status(200).json({
+    success: true,
+    message: 'Message Send Successfully!',
+  });
+};
